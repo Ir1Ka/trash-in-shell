ac23e44d2aa1d661eef49cc47cb5c6b16853dcca
IriKa <qiujie.jq@gmail.com>
2020-03-22 14:15:40 +0800

    Optmized install script

--- install.sh
+++ install.sh
@@ -4,24 +4,26 @@ DIR=$(cd $(dirname $0); pwd)
 TRASH_TOOL=.trashrc
 
 if [ -f "$DIR/$TRASH_TOOL" ]; then
-    cp $DIR/$TRASH_TOOL $HOME/
+    cp -uf "$DIR/$TRASH_TOOL" "$HOME"/
 else
     echo "$TRASH_TOOL is not exists"
     exit 1
 fi
 
 if [ -f "$HOME/.bashrc" ]; then
-    ENTRY_SCRIPT=$HOME/.bashrc
+    ENV_SCRIPT="$HOME/.bashrc"
 elif [ -f "$HOME/.profile" ]; then
-    ENTRY_SCRIPT=$HOME/.profile
+    ENV_SCRIPT="$HOME/.profile"
 else
-    echo "The current user does not have an env init script file"
-    echo -e "\tSuch as ~/.profile and ~/.bashrc"
+    echo "The current user does not have an env init script file" 1>&2
+    echo -e "\tSuch as ~/.bashrc or ~/.profile" 1>&2
     exit 1
 fi
 
-echo '' >> $ENTRY_SCRIPT
-echo '# include ~/.trashrc if it exists' >> $ENTRY_SCRIPT
-echo 'if [ -f "$HOME/.trashrc" ]; then' >> $ENTRY_SCRIPT
-echo '    . "$HOME/.trashrc"' >> $ENTRY_SCRIPT
-echo 'fi' >> $ENTRY_SCRIPT
+cat << EOF >> $ENV_SCRIPT
+
+# include ~/$TRASH_TOOL if it exists
+if [ -f "\$HOME/$TRASH_TOOL" ]; then
+    . "\$HOME/$TRASH_TOOL"
+fi
+EOF
